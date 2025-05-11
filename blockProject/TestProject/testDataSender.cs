0aa15f9bc88517317624239c7f297e8d0e209382
globusyel <yelyzavetailman@gmail.com>
2025-05-11 22:19:41 +0300

    fixed DataSender tests

--- blockProject/TestProject/testDataSender.cs
+++ blockProject/TestProject/testDataSender.cs
@@ -1,80 +1,77 @@
-﻿//using System.Net;
-//using System.Net.Sockets;
-//using System.Text;
-//using blockProject.blockchain;
-//using blockProject.httpServer;
-//using blockProject.nodeCommunicatio;
-//using blockProject.nodeCommunicatio.communicationFrames;
-//using Newtonsoft.Json;
-//using Xunit;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using blockProject.blockchain;
+using blockProject.nodeCommunicatio;
+using blockProject.randomSrc;
+//using Moq;
+using Newtonsoft.Json.Linq;
+using Xunit;
 
-//namespace testProject.testCommunication;
+namespace TestProject;
 
-//public class testDataSender
-//{
-//	private readonly List<Keys> _keys;
+public class DataSenderTests
+{
+	[Fact]
+	public void AddIP_ShouldAddNewIPEndPoint()
+	{
+		var sender = new DataSender();
+		var ip = new IPEndPoint(IPAddress.Loopback, 12345);
 
-//	public testDataSender()
-//	{
-//		_keys = Keys.LoadFromFile("testCommunication/.KeyFile");
-//	}
+		sender.AddIP(ip);
 
-//	[Fact]
-//	public async Task SendData_ShouldReturnNull_WhenSendingToMockServer()
-//	{
-//		var sender = new DataSender();
+		Assert.Contains(ip.ToString(), sender.GetIps());
+	}
 
-//		var listener = new TcpListener(IPAddress.Loopback, 0);
-//		listener.Start();
-//		var ip = (IPEndPoint)listener.LocalEndpoint;
-//		sender.AddIP(ip);
+	[Fact]
+	public void AddIP_ShouldNotAddDuplicateIPEndPoint()
+	{
+		var sender = new DataSender();
+		var ip = new IPEndPoint(IPAddress.Loopback, 12345);
 
-//		var message = new messageRecord(
-//			Convert.ToBase64String(_keys[1].PublicKey),
-//			Encoding.UTF8.GetBytes("test message"),
-//			_keys[0]
-//		);
+		sender.AddIP(ip);
+		sender.AddIP(ip);
 
-//		var serverTask = Task.Run(async () =>
-//		{
-//			using var client = await listener.AcceptTcpClientAsync();
-//			using var stream = client.GetStream();
+		Assert.Single(sender.GetIps());
+	}
 
-//			var buffer = new byte[4096];
-//			var read = await stream.ReadAsync(buffer);
-//			var received = Encoding.UTF8.GetString(buffer, 0, read);
+	[Fact]
+	public async Task SendData_ShouldReturnError_WhenDataIsNull()
+	{
+		var sender = new DataSender();
 
-//			var frame = JsonConvert.DeserializeObject<Frame>(received);
-//			Assert.NotNull(frame);
-//			Assert.Equal(Requests.ADD_RECORD, frame!.Request);
+		var result = await sender.SendData<object>(null!);
 
-//			var response = JsonConvert.SerializeObject(new Frame(Requests.ADD_RECORD, frame.data));
-//			var responseBytes = Encoding.UTF8.GetBytes(response);
-//			await stream.WriteAsync(responseBytes);
-//		});
+		Assert.NotNull(result);
+		Assert.Equal("empty data", result!.Message);
+	}
 
-//		var error = await sender.SendData(message);
 
-//		Assert.Null(error);
-//		listener.Stop();
-//	}
+	//[Fact]
+	//public async Task PingNode_ShouldReturnError_WhenNodeIsUnreachable()
+	//{
+	//	var sender = new DataSender();
+	//	sender.AddIP(new IPEndPoint(IPAddress.Loopback, 65000)); // Port zamknięty
 
-//	[Fact]
-//	public async Task SendData_ShouldReturnError_WhenServerNotAvailable()
-//	{
-//		var sender = new DataSender();
-//		var fakeIp = new IPEndPoint(IPAddress.Loopback, 9999);
-//		sender.AddIP(fakeIp);
+	//	var result = await sender.pingNode();
 
-//		var message = new messageRecord(
-//			Convert.ToBase64String(_keys[1].PublicKey),
-//			Encoding.UTF8.GetBytes("test message"),
-//			_keys[0]
-//		);
+	//	Assert.NotNull(result);
+	//	Assert.Contains("nie udało się wysłać danych", result!.Message);
+	//}
 
-//		var error = await sender.SendData(message);
+	[Fact]
+	public void GetIps_ShouldReturnFormattedIPs()
+	{
+		var sender = new DataSender();
+		var ip1 = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 1234);
+		var ip2 = new IPEndPoint(IPAddress.Parse("10.0.0.2"), 5678);
 
-//		Assert.NotNull(error);
-//		Assert.Contains("Błąd podczas wysyłania rekordu", error!.Message);
-//	}
-//}
+		sender.AddIP(ip1);
+		sender.AddIP(ip2);
+
+		var ips = sender.GetIps();
+
+		Assert.Contains("192.168.0.1:1234", ips);
+		Assert.Contains("10.0.0.2:5678", ips);
+	}
+}
